475836a2fe26771c7e70a03f9c1e6dd684c3919b
Elvis <houssouelvis@gmail.com>
2021-11-19 01:00:23 +0000

    la derniere

---
+++ js/manga.js
@@ -0,0 +1,18 @@
+//BARRE MENU RESPONSIVE
+const mainMenu = document.querySelector(".mainMenu");
+const closeMenu = document.querySelector(".closeMenu");
+const burger = document.querySelector(".burger");
+
+burger.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+function show() {
+  mainMenu.style.display = "initial";
+  mainMenu.style.right = "0";
+  mainMenu.style.transition = "1s"
+}
+
+function close() {
+  mainMenu.style.right = "-150%";
+  mainMenu.style.transition = "1s"
+}
